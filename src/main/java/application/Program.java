d2f7e9aba8b79749cdc87cf771c45e6fc38950ef
George <georgeneves.dev@gmail.com>
2023-09-30 23:29:35 -0300

    manipulando datas

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -3,37 +3,37 @@ package application;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class Program
 {
     public static void main( String[] args ){
 
-        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-
-        LocalDate d01 = LocalDate.now();
-        LocalDateTime d02 = LocalDateTime.now();
-        Instant d03 = Instant.now();
         LocalDate d04 = LocalDate.parse("2022-07-20");
         LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
         Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
 
-        LocalDate d08 = LocalDate.parse("20/07/2022", fmt1);
-        LocalDateTime d09 = LocalDateTime.parse("20/07/2022 01:30", fmt2);
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
+        DateTimeFormatter fmt4 = DateTimeFormatter.ISO_DATE_TIME;
+        DateTimeFormatter fmt5 = DateTimeFormatter.ISO_INSTANT;
 
-        LocalDate d10 = LocalDate.of(2022, 7, 20);
-        LocalDateTime d11 = LocalDateTime.of(2022, 7, 20, 01, 30);
 
-        System.out.println("D01 = " + d01.toString());
-        System.out.println("D02 = " + d02.toString());
-        System.out.println("D03 = " + d03.toString());
-        System.out.println("D04 = " + d04.toString());
-        System.out.println("D05 = " + d05.toString());
-        System.out.println("D06 = " + d06.toString());
-        System.out.println("D08 = " + d08.toString());
-        System.out.println("D09 = " + d09.toString());
-        System.out.println("D10 = " + d10.toString());
-        System.out.println("D10 = " + d11.toString());
+        System.out.println("D04: " + d04.format(fmt1));
+        System.out.println("D04: " + fmt1.format(d04));
+        System.out.println("D04: " + d04.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        System.out.println();
+
+        System.out.println("D05: " + d05.format(fmt1));
+        System.out.println("D05: " + d05.format(fmt2));
+        System.out.println("D05: " + d05.format(fmt4));
+        System.out.println();
+
+        System.out.println("D06: " + fmt3.format(d06));
+        System.out.println("D06: " + fmt5.format(d06));
+        System.out.println("D06: " + d06.toString());
+
     }
 }
